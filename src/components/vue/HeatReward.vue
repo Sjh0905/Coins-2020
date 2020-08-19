dafd9bed65810db90d173da572b3b0ef4975d3af
sunjiahui <2570167180@qq.com>
2020-08-19 15:03:17 +0800

    热度奖励

---
+++ src/components/vue/HeatReward.vue
@@ -0,0 +1,4 @@
+<template src="../html/HeatReward.html"></template>
+<style scoped src="../css/HeatReward.css"></style>
+<script src="../js/HeatReward.js"></script>
+<i18n src="../lang/HeatReward.json"></i18n>
