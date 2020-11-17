56b8d7aa1b42f8f6cd47eaae30595a03b7c1fed5
sunjiahui <2570167180@qq.com>
2020-11-17 11:35:13 +0800

    跟单时间修改

--- src/components/followTrade/js/MobileDocumentary.js
+++ src/components/followTrade/js/MobileDocumentary.js
@@ -278,7 +278,7 @@ root.methods.re_postDocumentaryImmediately = function (data) {
     data.errorCode == 12 &&  this.openPop(this.$t('用户余额不足'));
     data.errorCode == 15 &&  this.openPop(this.$t('冻结失败'));
     data.errorCode == 16 &&  this.openPop(this.$t('用户已经有仓位了不能跟随大神'));
-    data.errorCode == 17 &&  this.openPop(this.$t('用户和大神的杠杆倍数不一致'));
+    data.errorCode == 17 &&  this.openPop(this.$t('大神有仓位，暂不能跟单'));
     data.errorCode == 18 &&  this.openPop(this.$t('用户和大神的逐全仓模式不一致'));
     data.errorCode == 19 &&  this.openPop(this.$t('用户和大神的单双仓模式不一致'));
   }
