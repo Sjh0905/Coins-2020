13d3fd2ac81ed78cfaab18c2ac6c889770bcc145
sunjiahui <2570167180@qq.com>
2020-11-19 19:49:06 +0800

    最小交易额限制

--- src/components/js/ProgressBar.js
+++ src/components/js/ProgressBar.js
@@ -905,8 +905,8 @@ root.methods.RE_ERROR = function (err) {
 
   if(err_type == 'amount*price'){
 
-    txt = this.lang == 'CH' ? '低于最小交易额'+message.split("than")[1] + "\xa0" + 'USDT' || "最大值"
-      : 'Below the minimum transaction value: '+message.split("|")[1] + "\xa0" +'USDT' || "最大值";
+    txt = this.lang == 'CH' ? '交易额不能低于'+message.split("than")[1] + "\xa0" + 'USDT'
+      : 'Transaction volume cannot be lower than: '+message.split("|")[1] + "\xa0" +'USDT';
   }
   // console.warn("this is wrong", err)
   this.popText = txt;
