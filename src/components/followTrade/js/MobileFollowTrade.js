7f2f41eaccc73b4151f559be70c93c0ea1ce95ed
sunjiahui <2570167180@qq.com>
2020-11-23 11:53:49 +0800

    现货跟单，合约跟单更换位置

--- src/components/followTrade/js/MobileFollowTrade.js
+++ src/components/followTrade/js/MobileFollowTrade.js
@@ -22,7 +22,7 @@ root.data = function () {
     popIdenOpen: false,
 
     currentInterval:null,
-    switchOrder: 'SPOT',
+    switchOrder: 'CONTRACT',
   }
 }
 /*------------------------------ 生命周期 -------------------------------*/
@@ -40,8 +40,8 @@ root.data = function () {
   // });
 // }
 root.created = function () {
-  if(this.$route.query.isSwitchOrder == 'CONTRACT'){
-    this.switchOrder = 'CONTRACT'
+  if(this.$route.query.isSwitchOrder == 'SPOT'){
+    this.switchOrder = 'SPOT'
   }
 
   this.GET_AUTH_STATE()
